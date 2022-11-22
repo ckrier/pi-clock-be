2619e9c3bbde7ead0af43319a6efb576ad894d8b
ckrier <connorkrier@gmail.com>
2022-11-21 21:46:56 -0800

    handle spotify oauth login from the fe

--- sounds/sound_controller.py
+++ sounds/sound_controller.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, redirect
 
-from .spotify_playback_client import pause
+from .spotify_playback_client import pause, play, login
 
 sound_controller = Blueprint('sounds', __name__, url_prefix='/sounds')
 
@@ -10,3 +10,14 @@ sound_controller = Blueprint('sounds', __name__, url_prefix='/sounds')
 def stopSound():
     pause()
     return '', HTTPStatus.NO_CONTENT
+
+
+@sound_controller.route('/play', methods=['GET'])
+def playSound():
+    play()
+    return '', HTTPStatus.NO_CONTENT
+
+
+@sound_controller.route('/login', methods=['GET'])
+def login_endpoint():
+    return redirect(login(request.args.get("code")))
--- sounds/spotify_playback_client.py
+++ sounds/spotify_playback_client.py
@@ -1,7 +1,11 @@
 from os import environ
-from pprint import pprint
+import socket
+
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
+
+# ip to redirect to on the fe
+MACHINE_IP = environ['MACHINE_IP']
 
 # oauth configuration
 SPOTIFY_CLIENT_ID = environ['SPOTIFY_CLIENT_ID']
@@ -13,18 +17,41 @@ SPOTIFY_AUTH_SCOPES = ['user-modify-playback-state', 'user-read-playback-state',
 # device id to be used for playback
 SPOTIFY_DEVICE_ID = environ['SPOTIFY_DEVICE_ID']
 
+_auth_manager = SpotifyOAuth(
+    SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI,
+    scope=','.join(SPOTIFY_AUTH_SCOPES),
+    show_dialog=True
+)
+
+_spotify_client = Spotify(auth_manager=_auth_manager)
+
+
+def login(code):
+    if code is not None:
+        # Step 2. Being redirected from Spotify auth page
+        _auth_manager.get_access_token(code)
+        return f'http://{MACHINE_IP}:3000/settings'
+
+    if not _auth_manager.validate_token(_auth_manager.cache_handler.get_cached_token()):
+        # Step 1. Display sign in link when no token
+        return _auth_manager.get_authorize_url()
+
+    return f'http://{MACHINE_IP}:3000/settings'
+
 
 def play():
     playlist_uri = 'spotify:playlist:37i9dQZF1DWZd79rJ6a7lp'
 
-    client = __create_client__()
+    client = _spotify_client
 
     client.shuffle(True, SPOTIFY_DEVICE_ID)
     client.start_playback(SPOTIFY_DEVICE_ID, playlist_uri)
 
 
 def pause():
-    client = __create_client__()
+    client = _spotify_client
 
     if (is_playing(client)):
         client.pause_playback(SPOTIFY_DEVICE_ID)
@@ -32,20 +59,9 @@ def pause():
 
 def is_playing(client):
     if (client is None):
-        client = __create_client__()
+        client = _spotify_client
 
     playback = client.current_playback()
     return (playback is not None
             and playback['is_playing']
             and playback['device']['id'] == SPOTIFY_DEVICE_ID)
-
-
-def __create_client__():
-    auth = SpotifyOAuth(
-        SPOTIFY_CLIENT_ID,
-        SPOTIFY_CLIENT_SECRET,
-        redirect_uri=SPOTIFY_REDIRECT_URI,
-        scope=','.join(SPOTIFY_AUTH_SCOPES)
-    )
-
-    return Spotify(auth_manager=auth)
