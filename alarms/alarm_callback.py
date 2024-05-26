fd0cd8acb1bd153f2c9d5e5459e88780bbd6c24b
Connor Krier <ckrier@turo.com>
2024-05-26 02:13:06 +0000

    have sound fade in over the course of 15 minutes

--- alarms/alarm_callback.py
+++ alarms/alarm_callback.py
@@ -1,12 +1,26 @@
 import random
+import time
 from os import listdir, path
 from ..config import SOUND_FILE_DIR
 from ..sounds.file_playback_client import FilePlaybackClient
 
 
 def alarm_callback():
+    current_volume = 0
+    max_volume = 100
+    fade_time_seconds = 15 * 60
+    time_between_increment = fade_time_seconds / max_volume
+    
     sound = random.choice(listdir(SOUND_FILE_DIR))
-
     player = FilePlaybackClient()
+    
+    player.set_volume(current_volume)
     player.set_sound(path.join(SOUND_FILE_DIR, sound))
     player.play()
+
+    # increment volume by 1 in evenly spaced intervals
+    while current_volume <= max_volume:
+        current_volume += 1
+        player.set_volume(current_volume)
+
+        time.sleep(time_between_increment)
